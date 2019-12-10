1986390cd39933be300e32bd18edceace5a06db3
d.puzikov <d.puzikov@kingbird.ru>
2019-12-10 13:28:19 +0200

    Fix dependencies in home module

--- core-network-tmdblib-impl/src/main/java/com/example/moviesdb/network/impl/TMDBLibClientApiImpl.kt
+++ core-network-tmdblib-impl/src/main/java/com/example/moviesdb/network/impl/TMDBLibClientApiImpl.kt
@@ -3,6 +3,7 @@ package com.example.moviesdb.network.impl
 import com.example.moviesdb.network.api.TheMovieDBClientApi
 import com.example.moviesdb.network.model.Movie
 import info.movito.themoviedbapi.TmdbApi
+import info.movito.themoviedbapi.model.MovieDb
 import io.reactivex.Single
 import java.text.SimpleDateFormat
 import java.util.*
@@ -15,28 +16,41 @@ class TMDBLibClientApiImpl(
         private val RELEASE_DATE_PARSER = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
     }
 
+    override fun loadNowPlayingMovies(page: Int): Single<List<Movie>> {
+        return Single.fromCallable {
+            tmdbApi.movies
+                .getNowPlayingMovies("ru", page, "")
+                .results
+                .map { it.toMovie() }
+        }
+    }
+
     override fun loadPopularMovies(page: Int): Single<List<Movie>> {
         return Single.fromCallable {
             tmdbApi.movies.getPopularMovies("ru", page)
                 .results
-                .map { movieDb ->
-                    Movie(
-                        movieDb.posterPath,
-                        movieDb.isAdult,
-                        movieDb.overview,
-                        RELEASE_DATE_PARSER.parse(movieDb.releaseDate),
-                        movieDb.genres.map { genre -> genre.id },
-                        movieDb.id.toLong(),
-                        movieDb.originalTitle,
-                        movieDb.originalLanguage,
-                        movieDb.title,
-                        movieDb.backdropPath,
-                        movieDb.popularity,
-                        movieDb.voteCount.toLong(),
-                        false,
-                        movieDb.voteAverage
-                    )
-                }
+                .map { movieDb -> movieDb.toMovie() }
+        }
+    }
+
+    fun MovieDb.toMovie(): Movie {
+        this.apply {
+            return Movie(
+                posterPath,
+                isAdult,
+                overview,
+                RELEASE_DATE_PARSER.parse(releaseDate),
+                genres.map { genre -> genre.id },
+                id.toLong(),
+                originalTitle,
+                originalLanguage,
+                title,
+                backdropPath,
+                popularity,
+                voteCount.toLong(),
+                false,
+                voteAverage
+            )
         }
     }
 
--- feature-home-screen/src/main/java/com/example/feature/home/screen/api/HomeDependencies.kt
+++ feature-home-screen/src/main/java/com/example/feature/home/screen/api/HomeDependencies.kt
@@ -1,7 +1,10 @@
 package com.example.feature.home.screen.api
 
+import com.example.moviesdb.network.api.TheMovieDBClientApi
 import com.example.moviesdb.utils.ComponentDependencies
 
 interface HomeDependencies : ComponentDependencies {
+
+    fun tmdbClient(): TheMovieDBClientApi
 
 }
