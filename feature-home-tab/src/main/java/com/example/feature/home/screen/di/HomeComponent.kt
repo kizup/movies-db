306dec09d4d0218971222042dfbd4c240df69d7e
gaoyundexinmen@gmail.com <artem281996>
2019-12-19 17:54:32 +0400

    Add NavigatorModule to HomeComponent

--- feature-home-tab/src/main/java/com/example/feature/home/screen/di/HomeComponent.kt
+++ feature-home-tab/src/main/java/com/example/feature/home/screen/di/HomeComponent.kt
@@ -1,12 +1,13 @@
 package com.example.feature.home.screen.di
 
 import com.example.feature.home.screen.api.HomeDependencies
-import com.example.feature.home.screen.api.HomeNavigatorDependencies
 import com.example.feature.home.screen.presentation.view.HomeFragment
+import com.example.moviesdb.root.tab.di.modules.NavigatorModule
 import dagger.Component
 
 @Component(
-    dependencies = [ HomeDependencies::class, HomeNavigatorDependencies::class ]
+    dependencies = [HomeDependencies::class],
+    modules = [NavigatorModule::class]
 )
 interface HomeComponent {
 
